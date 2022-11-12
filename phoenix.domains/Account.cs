822122d2c3320d732c5fe21e6691a67747dde497
chicagodave <chicagodave42@gmail.com>
2022-11-11 22:39:03 -0600

    Added domains project and an architecture draft diagram.

---
+++ phoenix.domains/Account.cs
@@ -0,0 +1,10 @@
+﻿namespace phoenix.domains
+{
+    public class Account
+    {
+        private Guid id;
+        private string? handle;
+        private string? emailAddress;
+        private string? cellNumber;
+    }
+}
---
+++ phoenix.domains/Post.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace phoenix.domains
+{
+    internal class Post
+    {
+        private Guid id;
+        private Guid accountId;
+        private DateTimeOffset postDate;
+    }
+}
